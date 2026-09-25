3f124a1b4a1bfd7a4c0afea4e2ef7451b8223e2e
danialcala94 <danielalcalavalera@gmail.com>
2026-09-26 00:34:11 +0800

    Trying to fix the instagram download with a completely renewed code. Added test to manually test the download. New version.

--- tests/test_experimental.py
+++ tests/test_experimental.py
@@ -3,27 +3,47 @@ import pytest
 
 @pytest.mark.additional
 @pytest.mark.asyncio
-async def test_snapsave_app():
-    from social_media_downloader_easy.downloader.all.snapsaveapp import _SnapsaveDownloader
-    from tests.common import INSTAGRAM_REEL_URL
+async def test_instagram_downloader():
+    """
+    Due to changes in the endpoint we need
+    to make sure that we update the code
+    and it is working for the new version.
+    """
+    from social_media_downloader_easy import SocialMediaDownloader
 
-    download_url = _SnapsaveDownloader().download(INSTAGRAM_REEL_URL)
+    test_filename = 'test_files/test_instagram_downloader.mp4'
 
-    print(download_url)
+    await SocialMediaDownloader().download_video(
+        url = 'https://www.instagram.com/reel/DHtsmGHTOn4/',
+        output_filename = test_filename
+    )
 
-    # assert False
+    assert False
 
 
-# TODO: Too many requests sometimes...
-@pytest.mark.additional
-@pytest.mark.asyncio
-async def test_dl_arsya_biz_id():
-    from social_media_downloader_easy.downloader.all.dl_arsya_biz_id import _DlArsyaBizIdDownloader
-    from tests.common import INSTAGRAM_REEL_URL
+# @pytest.mark.additional
+# @pytest.mark.asyncio
+# async def test_snapsave_app():
+#     from social_media_downloader_easy.downloader.all.snapsaveapp import _SnapsaveDownloader
+#     from tests.common import INSTAGRAM_REEL_URL
 
-    download_url = await _DlArsyaBizIdDownloader().get_download_url(INSTAGRAM_REEL_URL)
+#     download_url = _SnapsaveDownloader().download(INSTAGRAM_REEL_URL)
 
-    print(download_url)
+#     print(download_url)
 
-    # assert False
+#     # assert False
+
+
+# # TODO: Too many requests sometimes...
+# @pytest.mark.additional
+# @pytest.mark.asyncio
+# async def test_dl_arsya_biz_id():
+#     from social_media_downloader_easy.downloader.all.dl_arsya_biz_id import _DlArsyaBizIdDownloader
+#     from tests.common import INSTAGRAM_REEL_URL
+
+#     download_url = await _DlArsyaBizIdDownloader().get_download_url(INSTAGRAM_REEL_URL)
+
+#     print(download_url)
+
+#     # assert False
     
